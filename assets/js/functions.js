9c82117956532566eff950529fa8bf121986e1ef
hani2284 <hani2284@colorado.edu>
2017-12-09 20:37:04 -0700

    VERSION 2

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,8 @@
+function preload() {
+    var img = loadImage('../img/sneaker4.gif');
+}
+
+
 $('a').hover(
        function(){ $(this).removeClass('hide') },
        function(){ $(this).addClass('hide') }
@@ -5,8 +10,10 @@ $('a').hover(
 
 $(".lasp-link").hover(
                function() {
+                   document.getElementById("learn-more-lasp").style.display = "block";
                    $( ".wrapper" ).addClass( "lasp" );
                }, function() {
+                   document.getElementById("learn-more-lasp").style.display = "none";
                    $( ".wrapper" ).removeClass( "lasp" );
                }
            );
@@ -21,8 +28,10 @@ $(".sneaker-link").hover(
 
 $(".userex-link").hover(
               function() {
+                    document.getElementById("learn-more-UX").style.display = "block";
                     $( ".wrapper" ).addClass( "ux" );
                 }, function() {
+                    document.getElementById("learn-more-UX").style.display = "none";
                     $( ".wrapper" ).removeClass( "ux" );
                 }
             );
@@ -37,22 +46,53 @@ $(".fashion-link").hover(
 
 $(".min-link").hover(
               function() {
+                    document.getElementById("learn-more-inspires").style.display = "block";
                     $( ".wrapper" ).addClass( "minimal" );
                 }, function() {
+                    document.getElementById("learn-more-inspires").style.display = "none";
                     $( ".wrapper" ).removeClass( "minimal" );
                 }
             );
 
 $(".design-link").hover(
               function() {
+                    document.getElementById("learn-more-inspires").style.display= "block";
                     $( ".wrapper" ).addClass( "design" );
                 }, function() {
+                    document.getElementById("learn-more-inspires").style.display = "none";
                     $( ".wrapper" ).removeClass( "design" );
                 }
             );
+
+
 $('design-link').hide();
 $('write-link').hide();
 $('min-link').hide();
 $('userex-link').hide();
 $('fashion-link').hide();
 $('sneaker-link').hide();
+
+$(".arrow-open").hover(
+    function() {
+        document.getElementById("arrow-holder").style.visibility = "visible";
+
+    }, function() {
+        document.getElementById("arrow-holder").style.visibility = "hidden";
+    }
+);
+
+
+function openNav(x) {
+
+    x.classList.toggle("change")
+
+    if (document.getElementById("sidenav").style.width != "0px" && document.getElementById("sidenav").style.width != "") {
+        document.getElementById("sidenav").style.width = "0px";
+        document.getElementById("main").style.marginLeft = "0px";
+    } else {
+        document.getElementById("sidenav").style.width = "200px";
+        document.getElementById("main").style.marginLeft = "200px";
+
+    }
+
+}
